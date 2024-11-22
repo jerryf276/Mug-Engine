a4716e31636d4370c49fab10d4ad5def034d1bd8
Voaly <55096594+Voaly@users.noreply.github.com>
2024-11-22 14:52:10 +0000

    edmma
    emma

---
+++ dependencies/raylib-master/out/build/x64-Debug/CTestTestfile.cmake
@@ -0,0 +1,8 @@
+# CMake generated Testfile for 
+# Source directory: C:/Users/voaly/Downloads/raylib-master/raylib-master
+# Build directory: C:/Users/voaly/Downloads/raylib-master/raylib-master/out/build/x64-Debug
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("raylib")
+subdirs("examples")
---
+++ dependencies/raylib-master/out/build/x64-Debug/raylib/CTestTestfile.cmake
@@ -0,0 +1,7 @@
+# CMake generated Testfile for 
+# Source directory: C:/Users/voaly/Downloads/raylib-master/raylib-master/src
+# Build directory: C:/Users/voaly/Downloads/raylib-master/raylib-master/out/build/x64-Debug/raylib
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("external/glfw")
